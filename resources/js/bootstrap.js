7933c5d58b1e16c8dc8c9802820c32c2854d3394
Alireza Behnamnik <alirezabehnamnik@gmail.com>
2020-07-18 15:21:10 +0430

    rtl-bootstrap 4

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -6,12 +6,12 @@ window._ = require('lodash');
  * code may be modified to fit the specific needs of your application.
  */
 
-try {
-    window.Popper = require('popper.js').default;
-    window.$ = window.jQuery = require('jquery');
+ try {
+     window.$ = window.jQuery = require('jquery');
+     window.Popper = require('popper.js');
 
-    require('bootstrap');
-} catch (e) {}
+     require('rtl-bootstrap');
+ } catch (e) {}
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
