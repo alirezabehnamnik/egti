aa8c9ef2f1d94cfe9af21e152906db1ca9a7ec5b
Alireza Behnamnik <alirezabehnamnik@gmail.com>
2020-07-19 22:41:38 +0430

    Add my games model

---
+++ app/MyGames.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MyGames extends Model
+{
+  /**
+   * Set table name
+   * @var string
+   */
+  protected $table = 'my_games';
+}
