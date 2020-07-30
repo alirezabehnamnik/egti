7d1bff96a948a7dff2c70f29b5a36d1f2fbc2bf4
Alireza Behnamnik <alirezabehnamnik@gmail.com>
2020-07-30 17:16:45 +0430

    tournaments results migration

---
+++ database/migrations/2020_07_30_093015_tournaments_results.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class TournamentsResults extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('tournaments_results', function (Blueprint $table) {
+          $table->id();
+          $table->unsignedBigInteger('tournament_id')->unique();
+          $table->foreign('tournament_id')->references('id')->on('tournaments');
+          $table->unsignedBigInteger('fplace_id');
+          $table->foreign('fplace_id')->references('id')->on('teams');
+          $table->unsignedBigInteger('splace_id');
+          $table->foreign('splace_id')->references('id')->on('teams');
+          $table->unsignedBigInteger('tplace_id');
+          $table->foreign('tplace_id')->references('id')->on('teams');
+          $table->unsignedBigInteger('foplace_id')->nullable();
+          $table->foreign('foplace_id')->references('id')->on('teams');
+          $table->unsignedBigInteger('fiplace_id')->nullable();
+          $table->foreign('fiplace_id')->references('id')->on('teams');
+          $table->unsignedBigInteger('enabled');
+          $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tournaments_results');
+    }
+}
