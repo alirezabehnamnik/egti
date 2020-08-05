423a0c70e3583cfb4285292885be7d759116a50e
Alireza Behnamnik <alirezabehnamnik@gmail.com>
2020-08-05 08:32:44 +0430

    tournaments register migration (not completed)

---
+++ database/migrations/2020_08_04_174029_tournaments_register.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class TournamentsRegister extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('tournaments_register', function (Blueprint $table) {
+          $table->id();
+          $table->unsignedBigInteger('tournament_id');
+          $table->foreign('tournament_id')->references('id')->on('tournaments');
+          $table->unsignedBigInteger('user_id');
+          $table->foreign('user_id')->references('id')->on('users');
+          $table->unsignedBigInteger('team_id');
+          $table->foreign('team_id')->references('id')->on('teams');
+          $table->unsignedBigInteger('enabled');
+          $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tournaments_register');
+    }
+}
