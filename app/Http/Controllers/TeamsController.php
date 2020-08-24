5492b523dcca932e71490153064b391a51ce5c52
Alireza Behnamnik <alirezabehnamnik@gmail.com>
2020-08-24 12:16:11 +0430

    bug fix

--- app/Http/Controllers/TeamsController.php
+++ app/Http/Controllers/TeamsController.php
@@ -14,11 +14,6 @@ use Illuminate\Support\Str;
 class TeamsController extends Controller
 {
 
-  public function __construct()
-  {
-      $this->middleware('auth');
-  }
-
   public function index($tag)
   {
     $data = Teams::where('tag', $tag)->first();
