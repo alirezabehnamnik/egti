125954b00a2c20a258579a0778c587d46a7e09d4
Alireza Behnamnik <alirezabehnamnik@gmail.com>
2020-09-11 19:36:54 +0430

    Add games Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(AdminSeeder::class);
         $this->call(MyGamesSeeder::class);
+        $this->call(GamesSeeder::class);
         $this->call(SettingSeeder::class);
     }
 }
---
+++ database/seeds/GamesSeeder.php
@@ -0,0 +1,89 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class GamesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('games')->insert(
+        [
+         'name' => 'Rainbow6 Siege',
+         'tag' => 'r6',
+         'enabled' => 1,
+         'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+         'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+       ]);
+
+       DB::table('games')->insert(
+         [
+          'name' => 'Counter-Strike: Global Offensive',
+          'tag' => 'csgo',
+          'enabled' => 1,
+          'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+          'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+
+        DB::table('games')->insert(
+          [
+           'name' => 'Overwatch',
+           'tag' => 'ow',
+           'enabled' => 1,
+           'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+           'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+         ]);
+
+         DB::table('games')->insert(
+           [
+            'name' => 'Player Unknown Battlegrounds',
+            'tag' => 'pubg',
+            'enabled' => 1,
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+          ]);
+
+          DB::table('games')->insert(
+            [
+             'name' => 'GTA V',
+             'tag' => 'gtav',
+             'enabled' => 1,
+             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+             'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+           ]);
+
+           DB::table('games')->insert(
+             [
+              'name' => 'Valorant',
+              'tag' => 'valorant',
+              'enabled' => 1,
+              'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+              'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ]);
+
+             DB::table('games')->insert(
+               [
+                'name' => 'FIFA',
+                'tag' => 'fifa',
+                'enabled' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+              ]);
+
+               DB::table('games')->insert(
+                 [
+                  'name' => 'Mortal Kombat 11',
+                  'tag' => 'mk',
+                  'enabled' => 1,
+                  'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                  'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                ]);
+
+
+
+    }
+}
